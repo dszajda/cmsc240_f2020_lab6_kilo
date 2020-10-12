9c9dbfb6b074f67c0c0ce2763f1b097f85be7a2a
mb5br <mb5br@mathcs01.richmond.edu>
2020-10-12 17:40:03 -0400

    Added content to methods in IntegerVector and corresponding tests in VectorTester

--- IntegerVector.cpp
+++ IntegerVector.cpp
@@ -10,37 +10,51 @@ IntegerVector::~IntegerVector() {}
 
 int IntegerVector::size() 
 {
-   return ...
+   return integerVector.size();
 }
 
 // just return the integer at the specified index; use the at() method rather
 // than [] so that an out_of_range error will be thrown for an invalid index
 int IntegerVector::get(int index)  
 { 
-   return ...
+   return integerVector.at(index);
 }
 
 // if index is size-legitimate, put the value at that index;
 // otherwise, use push_back to append to the end of the vector
 void IntegerVector::put(int value, int index)
 {
+	if (index >this->size() || index < 0){
+		integerVector.push_back(value);
+	}else{
+		integerVector.at(index) = value;
+	}
 }
 
 // use push_back to append
 void IntegerVector::put(int value)
 {
+	integerVector.push_back(value);
 }
 
 // for each char in characterVector, use static_cast<int> to append as an
 // integer to integerVector
 void IntegerVector::appendCharacterVector(CharacterVector& characterVector)
 {
+	for (int i = 0; i < characterVector.size(); i++){
+		int temp = static_cast<int>(characterVector.get(i));
+		integerVector.push_back(temp);
+	}
 }
 
 // for each double in doubleVector, use static_cast<int> to append as an
 // integer to integerVector
 void IntegerVector::appendDoubleVector(DoubleVector& doubleVector)
 {
+	for (int i = 0; i < doubleVector.size(); i++){
+		int temp = static_cast<int>(doubleVector.get(i));
+		integerVector.push_back(temp);
+	}	
 }
 
 #endif
--- VectorTester.cpp
+++ VectorTester.cpp
@@ -34,7 +34,56 @@ int main()
    std::cout << "--------------" << std::endl;
    std::cout << "IntegerVector:" << std::endl;
    std::cout << "--------------" << std::endl;
+   
+   std::cout << "Testing put(int value) Method:" << std::endl;
+   std::cout << "---------" << std::endl;
+   IntegerVector PutTest;
+   std::cout << "IntegerVector before put() method: " << std::endl;
+   std::cout << PutTest << std::endl;
+   PutTest.put(1);
+   PutTest.put(2);
+   PutTest.put(3);
+   std::cout << "IntegerVector after put() method: " << std::endl;
+   std::cout << PutTest << std::endl;
+   std::cout << "Expected : [1,2,3]" << std::endl;
 
+   std::cout << "Testing put(int value, int index) Method:" <<std::endl;
+   std::cout << "---------" << std::endl;
+   PutTest.put(1,1);
+   std::cout << PutTest << std::endl;
+   std::cout << "Expected : [1,1,3]" << std::endl;
+   
+   std::cout << "Testing get() Method:" << std::endl;
+   std::cout << "---------" << std::endl;
+   int getOne = PutTest.get(0);
+   std::cout << getOne << std::endl;
+   std::cout << "Expected: [1]" << std::endl;
+
+   std::cout << "Testing size() Method:" << std::endl;
+   std::cout << "---------" << std::endl;
+   std::cout << "Test on empty IntegerVector:" << std::endl;
+   IntegerVector empty;
+   int EmptySize = empty.size();
+   std::cout << EmptySize << std::endl;
+   std::cout << "Expected : [0]" << std::endl;
+   std::cout << "Test on IntegerVector with 3 elements:" << std::endl;
+   int ThreeSize = PutTest.size();
+   std::cout << ThreeSize << std::endl;
+   std::cout << "Expected : [3]" << std::endl;
+
+   std::cout << "Testing out_of_range: " << std::endl;
+   std::cout << "----------" << std::endl;
+   std::cout << "Testing put(int value, int index) at invalid index" << std::endl
+   PutTest.put(5,10);
+   std::cout << PutTest << std::endl;
+   std::cout << "Expected : [1,1,3,5]" << std::endl;
+   std::cout << "---------" << std::endl;
+   std::cout << "Testing get() method at invalid index:" << std::endl;
+   int getInvalid = PutTest.get(10);
+   std::cout << getInvalid << std::endl;
+   std::cout << "Expected : out_of_range error" << std::endl;
+   
+ 
    //-------------------------------------------------------------------------
 
    // using empty CharacterVector, test appending iv & dv from above
