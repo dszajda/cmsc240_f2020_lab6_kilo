728b8c24581c65e2e9d57c2361ddb71ed3a3b608
md5zr <md5zr@mathcs01.richmond.edu>
2020-10-13 17:21:32 -0400

    Updated files according to lab instrutions

--- DoubleVector.cpp
+++ DoubleVector.cpp
@@ -10,37 +10,50 @@ DoubleVector::~DoubleVector() {}
 
 int DoubleVector::size() 
 {
-   return ...
+   return doubleVector.size();
 }
 
 // just return the double at the specified index; use the at() method rather
 // than [] so that an out_of_range error will be thrown for an invalid index
 double DoubleVector::get(int index) 
 { 
-   return ...
+   return doubleVector.at(index);
 }
 
 // if index is size-legitimate, put the value at that index;
 // otherwise, use push_back to append to the end of the vector
 void DoubleVector::put(double value, int index)
 {
+   if(index < 0 || index > this->size()){
+	doubleVector.push_back(value);
+   }else{
+   	doubleVector.at(index) = value;
+   }
 }
-
 // use push_back to append
 void DoubleVector::put(double value)
 {
+     doubleVector.push_back(value);
 }
 
 // for each character in characteVector, use static_cast<double> to append as a
 // double to doubleVector
-void DoubleVector::appendCharacterVector(CharacterVector& characterVector)
+void DoubleVector::appendCharacterVector(CharacterVector&  characterVector)
 {
+	for(int i = 0; i < characterVector.size(); i++){
+		int temp = static_cast<double>(characterVector.get(i));
+		doubleVector.push_back(temp);
+        }
 }
-
 // for each integer in integerVector, use static_cast<double> to append as a
 // double to doubleVector
-void DoubleVector::appendIntegerVector(IntegerVector& integerVector)
+void DoubleVector::appendIntegerVector(IntegerVector&  integerVector)
 {
-}
+	for(int i = 0; i < integerVector.size(); i++){
+		int temp2 = static_cast<double>(integerVector.get(i));
+		doubleVector.push_back(temp2);
+        }  
+} 
+
 
 #endif
--- VectorTester.cpp
+++ VectorTester.cpp
@@ -27,6 +27,67 @@ int main()
    std::cout << "DoubleVector:" << std::endl;
    std::cout << "-------------" << std::endl;
 
+   std::cout << "Testing put(double value) method" << std::endl;
+   std::cout << "-------------" << std::endl;
+   DoubleVector PutTestDV;
+   std::cout << "DoubleVector before put() method: " << std::endl;
+   for(int i = 0; i < PutTestDV.size(); i ++){
+	std::cout << PutTestDV.get(i) << std::endl;
+   }
+   PutTestDV.put(1.0);
+   PutTestDV.put(2.0);
+   PutTestDV.put(3.0);
+   std::cout << "DoubleVector after put() method: " << std::endl;
+   for(int i = 0; i < PutTestDV.size(); i++){
+   	std::cout << PutTestDV.get(i) << std::endl;
+   }
+   std::cout << "Expected: [1.0, 2.0, 3.0]" << std::endl;
+   
+   std::cout <<"Testing put(double value, int index) method: " << std::endl;
+   std::cout << "-------------" << std::endl;
+   PutTestDV.put(1.0, 1);
+   for (int i = 0; i < PutTestDV.size(); i++){
+  	std::cout << PutTestDV.get(i) << std::endl;
+   } 
+   std::cout << "Expected: [1.0, 1.0, 3.0]" << std::endl;
+   
+   std::cout << "Testing get() method: " << std::endl;
+   std::cout << "-------------" << std::endl;
+   std::cout << PutTestDV.get(0) << std::endl;
+   std::cout << "Expected: [1.0]" << std::endl;
+
+   std::cout << "Testing size() method: " << std::endl;
+   std::cout << "-------------" << std::endl;
+   std::cout << "Test on an empty DoubleVector: " << std::endl;
+   DoubleVector emptyDV;
+   std::cout << emptyDV.size() << std::endl;
+   std::cout << "Expected : [0]" << std::endl;
+   std::cout << "Test on DoubleVector with 3 elements: " << std::endl;
+   std::cout << PutTestDV.size() << std::endl;
+   std::cout << "Expected : [3] " << std::endl;
+
+   std::cout << "Testing if out of range: " << std::endl;
+   std::cout << "-------------" << std::endl;
+   std::cout << "Testing put(double value, int index) invalid index" << std::endl;
+   PutTestDV.put(4.0, 10);
+   for(int i = 0; i < PutTestDV.size(); i++){
+   	std::cout << PutTestDV.get(i) << std::endl;
+   }
+   std::cout << "Expected: [1.0, 1.0, 3.0, 4.0]" << std::endl;
+   
+   std::cout << "Testing get() method with invalid index" << std::endl;
+   try{
+	std::cout << PutTestDV.get(10) << std::endl;
+   }catch(std::exception& e){
+   	std::cerr << "out_of_range error caught:" << std::endl;
+        std::cerr << e.what() << std::endl;
+   }
+
+  
+
+  
+
+
    //-------------------------------------------------------------------------
 
    // test IntegerVector: put, get, size, out_of_range
